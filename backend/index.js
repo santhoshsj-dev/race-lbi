3a5b71dfaf325fed9d0dbbd65b277e4de33bc295
santhoshsj-dev <santhoshsekar2903@gmail.com>
2024-03-14 10:37:47 +0530

    backenc url added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-import dotenv from "dotenv"
+const dotenv = require('dotenv');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
